13bf763de17b1b94d3e6bf401d695cc70dbd7168
Xi SONG <sungxisx@icloud.com>
2020-06-09 16:35:20 +0200

    Finish the backend part of FP1

--- app/Http/Controllers/DemandController.php
+++ app/Http/Controllers/DemandController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Demand;
+use App\ListUsers;
 use Illuminate\Http\Request;
 
 class DemandController extends Controller
 {
-    //
+    public function createDemande(Request $request) {
+        $user_id = $request->session()->get('userid');
+        $validatedData = $request -> validate([
+            'obj' => ['required', 'max:140'],
+            'env' => ['required', 'max:140'],
+            'numberUsers' => ['required', 'numeric'],
+            'listUsers' => ['required'],
+            'from' => ['required', 'date'],
+            'to' => ['required', 'date'],
+        ]);
+        $demand = new Demand;
+        $listUsers = new ListUsers;
+        $demand->user_id = $user_id;
+        $demand->description = $validatedData['env'];
+        $demand->numberUsers = $validatedData['numberUsers'];
+        $demand->fromDate = $validatedData['from'];
+        $demand->toDate = $validatedData['to'];
+        $demand->save();
+        $listUsers->demand_id = $demand->id;
+        $listUsers->users = $validatedData['listUsers'];
+        $listUsers->save();
+        return view('demande');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demande', 'UserController@addSession');
+
+Route::post('/newdemande', 'DemandController@createDemande');
